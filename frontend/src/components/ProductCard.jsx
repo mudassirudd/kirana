ca4b2574426d6de837d4a8547e711e21cb0ecfa6
mudassirudd <mudassirudd@gmail.com>
2025-11-26 20:56:36 +0530

    Add routing and product display features with React Router and HOME PAGE

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,14 @@
+export default function ProductCard({product}){
+  console.log(product.image)
+  return(
+    <>
+      <div className="product-card">
+        <img src={product.image} alt={product.description} />
+        <h3>{product.name}</h3>
+        <p>{product.description}</p>
+        <strong>${product.price}</strong>
+
+      </div>
+    </>
+  )
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ProductCard from '../components/ProductCard'
+import {Link} from 'react-router-dom'
+
+export default function Home(){
+    const [products,setProducts] = React.useState([])
+
+      React.useEffect(()=>{
+        async function fetchProducts() {
+          const res = await fetch('http://localhost:5000/products')
+          const data = await res.json()
+          setProducts( data )
+        }
+
+        fetchProducts()
+      },[])
+
+      
+      const productList = products.map(product=>(
+       <Link  to = {`/products/${product._id}`} key={product._id}> 
+           <ProductCard  product={product}/>
+           </Link>
+       
+      ))
+      console.log(products)
+
+  return(
+    <>
+      {productList}
+    </>
+  )
+}
