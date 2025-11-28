d7323229d753fe0cca8e703b8e400df28c1ce308
mudassirudd <mudassirudd@gmail.com>
2025-11-28 22:00:56 +0530

    Add registration page and form handling with validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Routes,Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
+import RegisterPage from './pages/RegisterPage'
 
 
 function App() {
@@ -11,10 +12,12 @@ function App() {
     <>
     <nav className='navbar'>
       <Link to='/'>Home</Link>
+      <Link to='/auth/register'>Register</Link>
     </nav>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/products/:id' element={<ProductPage/>}/>
+      <Route path='/auth/register' element={<RegisterPage/>}/>
     </Routes>
     </>
   )
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,59 @@
+
+import React from "react"
+
+export default function RegisterPage() {
+  const [error,setError] = React.useState(null)
+  const [success,setSuccess] = React.useState(null)
+
+
+
+  async function register(formData) {
+     setSuccess(null)
+     setError(null)
+
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log("Form submitted")
+
+    const res = await fetch("http://localhost:5000/auth/register",{
+      method:"POST",
+      body:JSON.stringify({email,password}),
+      headers:{
+        "Content-Type":"application/json"
+      }
+
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+      setError(data.error)
+      return
+    }
+    console.log("rssesponse:",data)
+    setSuccess("Registration Successful")
+  }
+ return(
+  
+   <div className="registration-form">
+    <h2>Register with us</h2>
+  <form action={register} > 
+  <label htmlFor="email">Email:
+  <input type="email" name="email"  required/>
+
+  </label>
+  <label htmlFor="password">Password:
+
+  <input type="password" name="password"  required/>
+  </label>
+  <button type="submit">Register</button>
+  
+ {error && <p style={{ color: "red" }}>{error}</p>}
+{success && <p style={{ color: "green" }}>{success}</p>}
+
+
+  </form>
+  </div>
+
+  
+ ) 
+}
